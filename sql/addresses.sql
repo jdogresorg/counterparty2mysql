577e9fdf23f7f57183461b24c254f830a0696400
J-Dog <j-dog@j-dog.net>
2017-09-30 14:58:08 -0700

    fix field comment

--- sql/addresses.sql
+++ sql/addresses.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS addresses;
 CREATE TABLE addresses (
     id             INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     address_id     VARCHAR(40) NOT NULL, -- id from index_addresses table
-    options        INTEGER UNSIGNED      -- address options
-    block_index    INTEGER UNSIGNED,     -- block that asset was created
+    options        INTEGER UNSIGNED,     -- address options
+    block_index    INTEGER UNSIGNED      -- block that option was specified
 ) ENGINE=MyISAM;
 
 CREATE UNIQUE INDEX address_id ON addresses (address_id);
