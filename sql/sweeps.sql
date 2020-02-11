36ed6414628e8064c2dcaba8d6ef01e8a7360ca2
J-Dog <j-dog@j-dog.net>
2020-02-10 16:00:45 -0800

    added sweeps table definition

---
+++ sql/sweeps.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS destructions;
+CREATE TABLE destructions (
+    tx_index    INTEGER UNSIGNED,
+    -- tx_hash  TEXT,
+    tx_hash_id  INTEGER UNSIGNED, -- id of record in index_transactions
+    block_index INTEGER UNSIGNED,
+    -- source   TEXT,
+    source_id   INTEGER UNSIGNED, -- id of record in index_addresses
+    -- asset    TEXT,
+    asset_id    INTEGER UNSIGNED, -- id of record in assets table
+    quantity    BIGINT,
+    tag         TEXT,
+    status      TEXT
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE UNIQUE INDEX tx_index    ON destructions (tx_index);
+CREATE        INDEX block_index ON destructions (block_index);
+CREATE        INDEX source_id   ON destructions (source_id);
+CREATE        INDEX asset_id    ON destructions (asset_id);
+CREATE        INDEX tx_hash_id  ON destructions (tx_hash_id);
