2023873b54d1b41773eebacedaa5bcf5e5b4971d
J-Dog <j-dog@j-dog.net>
2020-02-14 13:12:56 -0800

    fixed sweeps table definition

--- sql/sweeps.sql
+++ sql/sweeps.sql
@@ -1,20 +1,22 @@
-DROP TABLE IF EXISTS destructions;
-CREATE TABLE destructions (
-    tx_index    INTEGER UNSIGNED,
-    -- tx_hash  TEXT,
-    tx_hash_id  INTEGER UNSIGNED, -- id of record in index_transactions
-    block_index INTEGER UNSIGNED,
-    -- source   TEXT,
-    source_id   INTEGER UNSIGNED, -- id of record in index_addresses
-    -- asset    TEXT,
-    asset_id    INTEGER UNSIGNED, -- id of record in assets table
-    quantity    BIGINT,
-    tag         TEXT,
-    status      TEXT
+DROP TABLE IF EXISTS sweeps;
+CREATE TABLE sweeps (
+    tx_index       INTEGER UNSIGNED,
+    -- tx_hash     TEXT,
+    tx_hash_id     INTEGER UNSIGNED, -- id of record in index_transactions
+    block_index    INTEGER UNSIGNED,
+    -- source      TEXT,
+    source_id      INTEGER UNSIGNED, -- id of record in index_addresses
+    -- destination TEXT,
+    destination_id INTEGER UNSIGNED, -- id of record in index_addresses
+    -- asset       TEXT,
+    flags          INTEGER UNSIGNED, -- id of record in assets table
+    memo           BLOB,
+    fee_paid       INTEGER UNSIGNED, -- id of record in assets table
+    status         TEXT
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE UNIQUE INDEX tx_index    ON destructions (tx_index);
-CREATE        INDEX block_index ON destructions (block_index);
-CREATE        INDEX source_id   ON destructions (source_id);
-CREATE        INDEX asset_id    ON destructions (asset_id);
-CREATE        INDEX tx_hash_id  ON destructions (tx_hash_id);
+CREATE UNIQUE INDEX tx_index       ON sweeps (tx_index);
+CREATE        INDEX block_index    ON sweeps (block_index);
+CREATE        INDEX source_id      ON sweeps (source_id);
+CREATE        INDEX destination_id ON sweeps (destination_id);
+CREATE        INDEX tx_hash_id     ON sweeps (tx_hash_id);
