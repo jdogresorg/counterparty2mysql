7e1a4118d42a932b4039ae75fa954fccb2c779b3
J-Dog <j-dog@j-dog.net>
2021-07-08 12:10:17 -0700

    update dogepays table definition

--- sql/dogepays.sql
+++ sql/dogepays.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS btcpays;
-CREATE TABLE btcpays (
+DROP TABLE IF EXISTS dogepays;
+CREATE TABLE dogepays (
     tx_index       INTEGER UNSIGNED,
     -- tx_hash     TEXT,
     tx_hash_id     INTEGER UNSIGNED, -- id of record in index_transactions
@@ -8,13 +8,14 @@ CREATE TABLE btcpays (
     source_id      INTEGER UNSIGNED, -- id of record in index_addresses
     -- destination TEXT,
     destination_id INTEGER UNSIGNED, -- id of record in index_addresses
-    btc_amount     BIGINT,
+    doge_amount     BIGINT,
     order_match_id TEXT,
     status         TEXT
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE UNIQUE INDEX tx_index        ON btcpays (tx_index);
-CREATE        INDEX block_index     ON btcpays (block_index);
-CREATE        INDEX source_id       ON btcpays (source_id);
-CREATE        INDEX destination_id  ON btcpays (destination_id);
-CREATE        INDEX tx_hash_id      ON btcpays (tx_hash_id);
+CREATE UNIQUE INDEX tx_index        ON dogepays (tx_index);
+CREATE        INDEX block_index     ON dogepays (block_index);
+CREATE        INDEX source_id       ON dogepays (source_id);
+CREATE        INDEX destination_id  ON dogepays (destination_id);
+CREATE        INDEX tx_hash_id      ON dogepays (tx_hash_id);
+
