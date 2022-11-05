f130afa582a0b2d81c6e2f5167dc20ef6c0e2a12
J-Dog <j-dog@j-dog.net>
2022-11-05 16:23:34 -0700

    update to only pull list of assets from order matches and dispenses

--- misc/update_asset_prices.php
+++ misc/update_asset_prices.php
@@ -19,18 +19,57 @@ require_once(__DIR__ . '/../includes/config.php');
 initDB(DB_HOST, DB_USER, DB_PASS, DB_DATA, true);
 
 $assets = array(
-    'XCP',
-    // 'A10036924099664623395'
+    // 'A16471286040967332108'
 );
 
-$results = $mysqli->query("SELECT asset FROM assets WHERE btc_price IS NULL ORDER BY asset");
+// Get list of all assets from orders
+$sql = "SELECT 
+            a1.asset as forward_asset,
+            a2.asset as backward_asset
+        FROM 
+            order_matches m,
+            assets a1,
+            assets a2
+        WHERE
+            a1.id=m.forward_asset_id AND
+            a2.id=m.backward_asset_id AND
+            m.status='completed'";
+$results = $mysqli->query($sql);
 if($results){
     while($row = $results->fetch_assoc()){
-        array_push($assets,$row['asset']);
+        if(!in_array($row['forward_asset'], $assets))
+            array_push($assets,$row['forward_asset']);
+        if(!in_array($row['forward_asset'], $assets))
+            array_push($assets,$row['forward_asset']);
     }
 }
 
-foreach($assets as $asset){
-    print "Updating prices for {$asset}...\n";
+// Get list of all assets from dispenses
+$sql = "SELECT 
+            a.asset
+        FROM 
+            dispenses d,
+            assets a
+        WHERE
+            a.id=d.asset_id";
+$results = $mysqli->query($sql);
+if($results){
+    while($row = $results->fetch_assoc()){
+        if(!in_array($row['asset'], $assets))
+            array_push($assets,$row['asset']);
+    }
+}
+
+
+$total = count($assets);
+$cnt   = 0;
+sort($assets);
+foreach($assets as $idx => $asset){
+    $timer = new Profiler();
+    $cnt++;
+    print "[{$cnt}/{$total}] Updating prices for {$asset}...";
     updateAssetPrice($asset);
+    // Report time to process block
+    $time = $timer->finish();
+    print " Done [{$time}ms]\n";
 }
