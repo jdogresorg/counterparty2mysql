5ddb3ea212c28dbddce1673672c7790357156604
J-Dog <j-dog@j-dog.net>
2025-08-04 10:36:15 -0700

    add `mime_type` to broadcasts table

--- sql/v11-updates/updates.sql
+++ sql/v11-updates/updates.sql
@@ -1,4 +1,4 @@
--- v10.0.1 updates
+-- v11.0.0 updates
 
 -- fairminters table
 ALTER TABLE fairminters ADD max_mint_per_address VARCHAR(250);
@@ -6,3 +6,6 @@ ALTER TABLE fairminters ADD mime_type VARCHAR(250) DEFAULT "text/plain";
 
 -- issuances table
 ALTER TABLE issuances ADD mime_type VARCHAR(250) DEFAULT "text/plain";
+
+-- broadcasts table
+ALTER TABLE broadcasts ADD mime_type VARCHAR(250) DEFAULT "text/plain";
